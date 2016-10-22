d09823baea88264640497ac792c4ec99ff3d89fd
Bryant Ku <ku.bryant@gmail.com>
2016-10-22 17:56:41 -0400

    testing appspec

--- conf/start_application.sh
+++ conf/start_application.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
+
+msg $(pwd)
+msg $(ls -la)
+
 docker-compose -f $(dirname $0)/../docker-compose.dev.yml up -d
--- conf/stop_application.sh
+++ conf/stop_application.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
+
+msg $(pwd)
+msg $(ls -la)
 docker-compose -f $(dirname $0)/../docker-compose.dev.yml down
