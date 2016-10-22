97fa2eb864c2dc97d130f54179fccd7ca7216320
Bryant Ku <ku.bryant@gmail.com>
2016-10-22 19:35:36 -0400

    finally enable docker-compose

--- conf/start_application.sh
+++ conf/start_application.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-cat $(dirname $0)/../docker-compose.dev.yml
-# docker-compose -f $(dirname $0)/../docker-compose.dev.yml up -d
+docker-compose -f $(dirname $0)/../docker-compose.dev.yml up -d
--- conf/stop_application.sh
+++ conf/stop_application.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-cat $(dirname $0)/../docker-compose.dev.yml
-
-# docker-compose -f $(dirname $0)/../docker-compose.dev.yml down
+docker-compose -f $(dirname $0)/../docker-compose.dev.yml down
