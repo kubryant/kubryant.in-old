98db374d98856a4a16d4cace5e34009a1fba9314
Bryant Ku <ku.bryant@gmail.com>
2016-10-30 22:00:02 -0400

    pause for dhparams

--- conf/start_application.sh
+++ conf/start_application.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 hash docker-compose 2>/dev/null || sleep 60
+cat ../certs/dhparams.pem 2>dev/null || sleep 120
 
 docker-compose -f $(dirname $0)/../docker-compose.production.yml up --build -d > docker-kubryant.log
