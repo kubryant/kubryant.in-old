5dfd41abb79b170d8263066767bcd2f82546dae4
Bryant Ku <ku.bryant@gmail.com>
2016-10-30 22:26:18 -0400

    fix pathing

--- conf/start_application.sh
+++ conf/start_application.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+cd /webapp
 
 hash docker-compose 2>/dev/null || sleep 60
-cat ../certs/dhparams.pem 2>/dev/null || sleep 120
+cat certs/dhparams.pem 2>/dev/null || sleep 120
 
-docker-compose -f $(dirname $0)/../docker-compose.production.yml up --build -d > docker-kubryant.log
+docker-compose -f docker-compose.production.yml up --build -d > docker-kubryant.log
